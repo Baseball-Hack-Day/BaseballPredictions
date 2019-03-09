59aa9021b9f140ee8a87e81653c75fc3a8f72a8b
Connie Lim <jlim13@binghamton.edu>
2019-03-09 13:58:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { players } from './stats'
 
 class App extends Component {
+
   render() {
+
+    let playersInfo = players.map(player => <div>{player.nameFirst} {player.nameLast}</div>)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>List of Players for MiddleBury School District</h1>
+        {playersInfo}
       </div>
     );
   }
