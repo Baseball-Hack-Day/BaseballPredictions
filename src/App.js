d4abe8f05bee7418d8c940d852082cbe916df3c6
Connie Lim <jlim13@binghamton.edu>
2019-03-09 14:32:23 -0500

    added banner

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.css';
 import { players } from './stats'
 
 class App extends Component {
 
   render() {
 
-    let playersInfo = players.map(player => <div>{player.nameFirst} {player.nameLast}</div>)
+    let playersInfo = players.map(player => <div key={player.playerID}>{player.nameFirst} {player.nameLast}</div>)
 
     return (
-      <div className="App">
-        <h1>List of Players for MiddleBury School District</h1>
-        {playersInfo}
+      <div className="main-container">
+        <div className="header">
+          <h1>MLB GameChanger Mobile App</h1>
+        </div>
+        
+        <div className="banner">
+          <img src="https://www.baseballtradingpins.net/wp-content/uploads/2018/04/Youth-Baseball-1024x682.jpeg" />
+        </div>
+        
+        <div className="players-container">
+          <h1>List of Players for MiddleBury School District</h1>
+          {playersInfo}
+        </div>
       </div>
     );
   }
